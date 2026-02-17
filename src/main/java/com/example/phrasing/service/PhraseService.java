a9b46f5fb742e3d9c50755e97fe1df8832c0c45e
orestisLyk <orestis.lyk@outlook.com>
2026-02-17 13:41:03 +0200

    fix: fixed orElseThrow() lambda in getAllByUser in PhraseService

--- src/main/java/com/example/phrasing/service/PhraseService.java
+++ src/main/java/com/example/phrasing/service/PhraseService.java
@@ -4,6 +4,7 @@ import com.example.phrasing.model.Phrase;
 import com.example.phrasing.model.User;
 import com.example.phrasing.repository.PhraseRepository;
 import com.example.phrasing.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,10 +25,9 @@ public class PhraseService {
         return phraseRepository.findAllByOrderByCreatedAtDesc(pageable);
     }
 
-    public Page<Phrase> getAllByUser(Long userId, Pageable pageable) {
-        try {
-            User user = userRepository.findById(userId)
-                    .orElseThrow();
-        }
+    public Page<Phrase> getAllByUser(Long userId, Pageable pageable) throws EntityNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return phraseRepository.findByUser(user,  pageable);
     }
 }
